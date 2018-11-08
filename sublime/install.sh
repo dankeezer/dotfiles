6a1ebd3f3e3feaff8d4e036ae93ea6603666846a
dankeezer <dankeezer@gmail.com>
2018-11-08 18:02:32 -0500

    fix space, gitignore cache

--- sublime/install.sh
+++ sublime/install.sh
@@ -3,7 +3,7 @@
 echo
 echo Installing Sublime Text 3 Custom Packages and Settings...
 
-curl -o /Users/$(whoami)/Library/Application\ Support/Sublime\ Text\ 3/Installed\ Packages/Package%20Control.sublime-package https://packagecontrol.io/Package%20Control.sublime-package
+curl -o /Users/$(whoami)/Library/Application\ Support/Sublime\ Text\ 3/Installed\ Packages/Package\ Control.sublime-package https://packagecontrol.io/Package%20Control.sublime-package
 
 git clone https://github.com/SideBarEnhancements-org/SideBarEnhancements.git /Users/$(whoami)/Library/Application\ Support/Sublime\ Text\ 3/Packages/SideBarEnhancements
 git clone https://github.com/p3lim/sublime-highlight-trailing-whitespace /Users/$(whoami)/Library/Application\ Support/Sublime\ Text\ 3/Packages/Highlight\ Trailing\ Whitespace
