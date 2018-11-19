69218f80a45bbb927c171c69db15b0d684e5aebc
dankeezer <dankeezer@gmail.com>
2018-11-19 16:27:09 -0500

    more sublime refactor

--- sublime-text/install.sh
+++ sublime-text/install.sh
@@ -2,36 +2,66 @@
 
 if [[ "$(uname -s)" = "Darwin" ]]
 then
-  SUBLIME_BASE_PATH="$HOME/Library/Application\ Support/Sublime\ Text\ 3"
+  SUBLIME_BASE_PATH="$HOME/Library/Application Support/Sublime Text 3"
   echo
   echo "Installing Sublime Text 3 Custom Packages and Settings..."
 
+  make_directories () {
+    # Sublime Text will automatically make these directories upon first launch
+    # But we're making changes prior to first launch
+
+    if ! [ -d "$SUBLIME_BASE_PATH/Packages/User" ]
+    then
+      mkdir -p "$SUBLIME_BASE_PATH/Packages/User"
+    fi
+
+    if ! [ -d "$SUBLIME_BASE_PATH/Installed Packages" ]
+    then
+      mkdir -p "$SUBLIME_BASE_PATH/Installed Packages"
+    fi
+  }
+
+  install_package_control () {
+    if ! [ -f "$SUBLIME_BASE_PATH/Installed Packages/Package Control.sublime-package" ]
+    then
+      curl -o "$SUBLIME_BASE_PATH/Installed Packages/Package Control.sublime-package" \
+        https://packagecontrol.io/Package%20Control.sublime-package
+    else
+      echo "skipping Package Control, already installed"
+    fi
+  }
+
   clone_repo () {
-    local repo="https://github.com/$1" dir=$2
+    local repo=$1 dir=$2
 
     if ! [ -d "$SUBLIME_BASE_PATH/Packages/$dir" ]
     then
-      git clone "$repo" "$SUBLIME_BASE_PATH/Packages/$dir"
+      git clone "https://github.com/$repo" "$SUBLIME_BASE_PATH/Packages/$dir"
     else
       echo "skipping $dir, already cloned"
     fi
   }
 
+  symlink_user_settings () {
+    if [ -f "$SUBLIME_BASE_PATH/Packages/User/Preferences.sublime-settings" ]
+    then
+      cp "$SUBLIME_BASE_PATH/Packages/User/Preferences.sublime-settings" \
+        "$SUBLIME_BASE_PATH/Packages/User/Preferences.sublime-settings-old"
+      rm "$SUBLIME_BASE_PATH/Packages/User/Preferences.sublime-settings"
+    fi
+
+    ln -s ~/.dotfiles/sublime-text/Preferences.sublime-settings \
+      "$SUBLIME_BASE_PATH/Packages/User/Preferences.sublime-settings"
+    fi
+  }
+
+  make_directories
+  install_package_control
+  symlink_user_settings
+
   clone_repo 'SideBarEnhancements-org/SideBarEnhancements.git' 'SideBarEnhancements'
-  clone_repo 'p3lim/sublime-highlight-trailing-whitespace' 'Highlight\ Trailing\ Whitespace'
+  clone_repo 'p3lim/sublime-highlight-trailing-whitespace' 'Highlight Trailing Whitespace'
   clone_repo 'slim-template/ruby-slim.tmbundle.git' 'ruby-slim.tmbundle'
-
-  if ! [ -f "$SUBLIME_BASE_PATH/Installed\ Packages/Package\ Control.sublime-package" ]
-  then
-    sudo mkdir "$SUBLIME_BASE_PATH/Installed\ Packages"
-    sudo curl -o "$SUBLIME_BASE_PATH/Installed\ Packages/Package\ Control.sublime-package" \
-      https://packagecontrol.io/Package%20Control.sublime-package
-  else
-    echo "skipping Package Control, already installed"
-  fi
-
-	rm -rf "$SUBLIME_BASE_PATH/Packages/User"
-	ln -s ~/.dotfiles/sublime/User/ "$SUBLIME_BASE_PATH/Packages/User"
 else
   echo
   echo "Not macOS, skipping Sublime Text 3 Custom Packages and Settings..."
